5aac95988c7c5bf4454a9443b65c4a3677fb304d
prakash3113 <prakash.raje7@gmail.com>
2021-01-10 19:58:46 +0530

    my first reactwebpage

---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return(
+        <>
+       <footer className="w-100 bg-light text-center">
+         <br></br>  <p> @2021 BSPAK. All Rights Reserved | Terms And Conditions</p>
+       </footer>
+        </>
+    
+    );
+};
+export default Footer;
---
+++ src/Sdata.jsx
@@ -0,0 +1,36 @@
+import web from "../src/images/web.jpeg";
+import app from "../src/images/app.jpg";
+import android from "../src/images/android.jpg";
+import digital from "../src/images/dmark.jpg";
+import marketing from "../src/images/dmark2.png";
+import software from "../src/images/software.jpg";
+
+const Sdata = [
+    {
+        imgsrc: web,
+        title: "web developement",
+    },
+    {
+        imgsrc: app,
+        title: "app developement",
+    },
+    {
+        imgsrc: software,
+        title: "software developement",
+    },
+    {
+        imgsrc: digital,
+        title: "digital developement",
+    },
+    {
+        imgsrc: marketing,
+        title: "marketing developement",
+    },
+    {
+        imgsrc: android,
+        title: "android developement",
+    },
+   
+];
+
+export default Sdata;
